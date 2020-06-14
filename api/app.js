c3cc5018a68dac16537469b7fde26664b56392ce
S1mpler <stas.dior@gmail.com>
2020-06-14 19:11:28 +0200

    Add main video background

---
+++ api/app.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+/////////////////////////////////////////////////////////
+// Configuration
+/////////////////////////////////////////////////////////
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+app.use(cors());
+app.use(express.static(__dirname + '/public'));
+
+mongoose
+  .connect(process.env.DB_HOST, {
+    useNewUrlParser: true,
+    promiseLibrary: global.Promise,
+  })
+  .then((status) => {
+    console.info(`DB: connected to: ${process.env.DB_HOST}`);
+  })
+  .catch((err) => {
+    console.error(`DB: failed to connect to: ${process.env.DB_HOST}`);
+  });
+
+/////////////////////////////////////////////////////////
+// Routes
+/////////////////////////////////////////////////////////
+
+const apiRouter = require('./routes/api-router');
+
+/////////////////////////////////////////////////////////
+// Middlewares
+/////////////////////////////////////////////////////////
+
+// const myMiddleware = require("./middleware/my-middleware");
+
+/////////////////////////////////////////////////////////
+// App Use
+/////////////////////////////////////////////////////////
+
+app.use('/api', apiRouter);
+
+// app.use("/myRoute", myMiddleware, myRoute);
+
+/////////////////////////////////////////////////////////
+// Start Server
+/////////////////////////////////////////////////////////
+
+const port = process.env.PORT || 80;
+
+app.listen(port, () => {
+  console.log(`Listening to the port ${port}`);
+});
--- client/src/app/intro/components/intro/intro.component.ts
+++ client/src/app/intro/components/intro/intro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,11 +7,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./intro.component.scss'],
 })
 export class IntroComponent implements OnInit {
+  @ViewChild('backgroundVideo', { static: true }) backgroundVideo: ElementRef<
+    HTMLVideoElement
+  >;
+
+  public hideText = false;
+
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
-  public startTutorials(): void {
+  public onStartClicked(): void {
+    this.hideText = true;
+    this.backgroundVideo.nativeElement.play();
+  }
+
+  public redirectToTutorials(): void {
     this.router.navigate(['tutorials']);
   }
 }
